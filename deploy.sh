f942321308b0141ba614823c395bbcdb22cf9ea4
Elad Shuster <selad@paloaltonetworks.com>
2020-09-23 14:55:29 +0300

    added deployment files

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t e-shuster/multi-client:latest -t e-shuster/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t e-shuster/multi-server:latest -t e-shuster/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t e-shuster/multi-worker:latest -t e-shuster/multi-worker:$SHA -f ./worker/Dockrfile ./worker
+docker build -t eshuster/multi-client:latest -t eshuster/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t eshuster/multi-server:latest -t eshuster/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t eshuster/multi-worker:latest -t eshuster/multi-worker:$SHA -f ./worker/Dockrfile ./worker
 
-docker push e-shuster/multi-client:latest
-docker push e-shuster/multi-server:latest
-docker push e-shuster/multi-worker:latest
+docker push eshuster/multi-client:latest
+docker push eshuster/multi-server:latest
+docker push eshuster/multi-worker:latest
 
-docker push e-shuster/multi-client:$SHA
-docker push e-shuster/multi-server:$SHA
-docker push e-shuster/multi-worker:$SHA
+docker push eshuster/multi-client:$SHA
+docker push eshuster/multi-server:$SHA
+docker push eshuster/multi-worker:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=eshsuter/multi-server:$SHA
-kubectl set image deployments/client-deployment client=e-shuster/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=e-shuster/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=eshuster/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=eshuster/multi-worker:$SHA
